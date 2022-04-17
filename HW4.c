63bc518e7d3df0573b21a58323d14d2ca40742f7
bhaskargopati1997 <67636341+bhaskargopati1997@users.noreply.github.com>
2022-04-17 18:21:24 -0500

    Added function declarations.

--- HW4.c
+++ HW4.c
@@ -11,3 +11,11 @@
 #define SUBMIT_CMD "submit"
 #define SHOW_JOBS "showjobs"
 #define SUBMIT_HISTORY "submithistory"
+
+int max_jobs = 0;
+int running_jobs = 0;
+
+void run_job_scheduler();
+void exec_job(struct Job *job);
+void submit_job(char *arr[], int size, char *command);
+void process_command(char *command);
